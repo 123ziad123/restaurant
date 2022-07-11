5e8fcb99bb3adf3edb79c668a129dfe2cc7f31a2
ziad <ziadamer102030@gmail.com>
2022-07-10 21:48:01 -0700

    modifiy gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,69 +1,47 @@
-import React, { useRef } from 'react';
-import { images, data } from '../../constants';
+import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
 import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Gallery.css';
 
-function Gallery() {
-  
-  const GalleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04]
-  const GalleryImagesReversed = [images.gallery04, images.gallery03, images.gallery02, images.gallery01]
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
 
-  const imgsContainer1 = useRef()
-  const imgsContainer2 = useRef()
-  
-  window.onscroll = function () {
-    let value = window.scrollY
-    imgsContainer1.current.style.left = value / -6  + 'px'
-    imgsContainer2.current.style.right = value / -6  + 'px'
-  }
-  
-  
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
   return (
     <div className="app__gallery flex__center">
-      <div className="app__gallery-content" data-aos="fade-down" data-aos-duration="400">
+      <div className="app__gallery-content" data-aos="fade-right" data-aos-duration="400">
         <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
-        <p className="p__opensans" style={{ color: '#AAA', marginTop: '1rem' }}>Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Volutpat Mattis Ipsum Turpis Elit.</p>
-        <button type='button' className='custom__button' style={{ marginBlock: '1rem 2rem' }}>View More</button>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
       </div>
-      <div className="app__gallery-imgs">
-        <div className="app__gallery-imgs_container one" ref={imgsContainer1}>
-          {GalleryImages.map((img, index) => (
-            <div className="app__gallery-imgs_card flex__center" key={index}>
-              <img src={ img } alt="img" />
-            </div>
-          ))}
-          {GalleryImages.map((img, index) => (
-            <div className="app__gallery-imgs_card flex__center" key={index}>
-              <img src={ img } alt="img" />
-            </div>
-          ))}
-          {GalleryImages.map((img, index) => (
-            <div className="app__gallery-imgs_card flex__center" key={index}>
-              <img src={ img } alt="img" />
+      <div className="app__gallery-images" data-aos="fade-left" data-aos-duration="400">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
             </div>
           ))}
         </div>
-        <div className="app__gallery-imgs_container two" ref={imgsContainer2}>
-          {GalleryImagesReversed.map((img, index) => (
-            <div className="app__gallery-imgs_card flex__center" key={index}>
-              <img src={ img } alt="img" />
-            </div>
-          ))}
-          {GalleryImagesReversed.map((img, index) => (
-            <div className="app__gallery-imgs_card flex__center" key={index}>
-              <img src={ img } alt="img" />
-            </div>
-          ))}
-          {GalleryImagesReversed.map((img, index) => (
-            <div className="app__gallery-imgs_card flex__center" key={index}>
-              <img src={ img } alt="img" />
-            </div>
-          ))}
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Gallery;
